fb744626462cc1babfe4ac2b58fff2d3de42f674
sohrabProton <131691331+sohrabProton@users.noreply.github.com>
2023-04-24 18:48:14 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='19b0fe05-cb99-4b64-871b-c8885c0d5aed'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
